cce73b481dc0c09693746487420b60eb5b7931cc
MARCUSS SUDARIO <msudario131@gmail.com>
2025-10-20 04:05:24 +0000

    misspelled and incomplete syntax done

---
+++ HelloWorldApp.java
@@ -0,0 +1,10 @@
+public class HelloWorldApp { 
+/*incomplete code & misspelled of "class" 
+*helloWorldApp change into HelloWorldApp
+*/
+    public static void main (String [] args) {
+        System.out.println("Kamusta Mundo!"); //letter p is missing in println syntax
+
+    }
+    
+}
