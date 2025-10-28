c54afec034dae5848e4677af2781a6e3905a2ad4
MARCUSS SUDARIO <msudario131@gmail.com>
2025-10-28 15:20:51 +0000

    task-sheet-1-1-2

--- Task sheet 1-1-1/Main.java
+++ Task sheet 1-1-1/Main.java
@@ -27,3 +27,27 @@ class Plant{
         return branch;
     }
 }
+
+class Toy {
+    String name;
+    String brand;
+    double price;
+    int qty;
+
+    void setPrice (double price) {
+            this.price = price;
+    }
+
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+
+        toy1.name = "Car";
+        toy1.brand = "mitsu";
+        toy1.price = 5000;
+        toy1.qty = 3;
+        toy1.setPrice((toy1.price*0.5));
+
+        System.out.println(());
+
+    }
+}
