57f394d00a55466a80c30b522c605c55d8a7e359
MARCUSS SUDARIO <msudario131@gmail.com>
2025-11-05 04:01:34 +0000

    arraylesson

---
+++ chapt3/ArrayKo.java
@@ -0,0 +1,25 @@
+package chapt3;
+
+public class ArrayKo {
+    public static void main(String[] args) {
+        int[] i = new int[3];
+        int[] numbers2 = new int[] {1, 54, 75};
+        int[] numbers3 = {1, 54, 75};
+        Student[] batch2 ={new Student(),new Student(),new Student(),new Student(),null};
+        for(int j = 0; j<=numbers3.length;j++)
+            System.out.println(numbers3[j]);
+
+    }
+    
+}
+
+class Student {
+    public static void main(String[] args) {
+        String fullname;
+        int age;
+        String address;
+        String course;
+        int yearlevel;
+    }
+}
+
--- chapt3/StringLesson.java
+++ chapt3/StringLesson.java
@@ -14,9 +14,9 @@ public class StringLesson {
         String name3 = new String("fluffy");
 
             System.out.println(a);
-            System.out.println(name3.indexOf('l'));
-            System.out.println(name3.length());
-            System.out.println(name3.substring(3));
+            System.out.println(name3.indexOf('l')); //index of
+            System.out.println(name3.length()); // length
+            System.out.println(name3.substring(3)); 
             System.out.println(name3.substring(3,4));
             System.out.println(name3.substring(3,5));
 
@@ -35,6 +35,77 @@ public class StringLesson {
             System.out.println("Marcuss".replace("Marcuss", "pogi"));
             System.out.println("\t   a b x\n".trim());
 
+
+
+
         }
     }
 
+    class Sbuilder {
+        public static void main(String[] args) {
+            
+            String alpha = "";
+
+            for(char current ='a'; current<='z'; current++)
+            alpha +=current;
+            System.out.println(alpha);
+
+            
+            //mutability and chaining
+            StringBuilder sb = new StringBuilder("start");
+            System.out.println(sb);
+            sb.append("+middle");
+            System.out.println(sb);
+            StringBuilder same = sb.append("+end");
+            System.out.println(sb);
+            System.out.println(same);
+
+            StringBuilder abc = new StringBuilder("animals");
+            String sub = abc.substring(abc.indexOf("a", abc.indexOf("als")));
+            int len = abc.length();
+            char ch = abc.charAt(6);
+            System.out.println(sub + " " + len + " " + ch);
+
+            // append(String str) using it w/out comment
+            // insert() adds characters to the StringBuilder at the requested index.
+            // delete(int start, int end) / deleteCharAt(int index)
+            //reverse()
+            // String toString()
+
+            String s = sb.toString();
+            System.out.println(s);
+
+            same.reverse();
+            System.out.println(same);
+
+            //Understanding Equality
+
+            StringBuilder one = new StringBuilder();
+             StringBuilder two = new StringBuilder();
+              StringBuilder three =one.append("a");
+              System.out.println(one == two);
+              System.out.println(one == three);
+
+            String x = "Hello World";
+            String z = " Hello World".trim();
+            System.out.println(x == z); // false
+
+            String e = "Hello World";
+            String f = " Hello World".trim();
+            System.out.println(e.equals(z)); // true
+        
+        }
+    }
+
+    class Tiger {
+        public static void main(String[] args) {
+            Tiger t1 = new Tiger();
+            Tiger t2 = new Tiger();
+            Tiger t3 = t1;
+
+            System.out.println(t1 == t1); // true
+            System.out.println(t1 == t2); // false
+            System.out.println(t1.equals(t2)); // true
+
+        }
+    }
